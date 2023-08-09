ed7e6729f0fc4a7a03f066b43c8b274d2ae0f5d9
Oleksandr Cherevko <oc.oleks.cherevko@gmail.com>
2023-08-09 21:19:20 +0300

    task4 has been done

---
+++ Task_4/CreditCard.cs
@@ -0,0 +1,98 @@
+﻿namespace Task_4
+{
+    public class CreditCard
+    {
+        private string _cardNumber;
+        private int _cvcCode;
+        private decimal _balance;
+
+        public string CardNumber
+        {
+            get { return _cardNumber; }
+            set { _cardNumber = value; }
+        }
+
+        public int CVCCode
+        {
+            get { return _cvcCode; }
+            set
+            {
+                if (value >= 100 && value <= 999)
+                    _cvcCode = value;
+                else
+                    throw new ArgumentOutOfRangeException("The CVC code must be a three-digit number");
+            }
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        public CreditCard(string cardNumber, int cvcCode, decimal balance)
+        {
+            this._cardNumber = cardNumber;
+            this._cvcCode = cvcCode;
+            this._balance = balance;
+        }
+
+
+        public static CreditCard operator +(CreditCard card, decimal amount)
+        {
+            card._balance += amount;
+            return card;
+        }
+
+
+        public static CreditCard operator -(CreditCard card, decimal amount)
+        {
+            if (card._balance < amount)
+                throw new InvalidOperationException("Not enough money on the card");
+
+            card._balance -= amount;
+            return card;
+        }
+
+
+        public static bool operator ==(CreditCard card1, CreditCard card2)
+        {
+            if (ReferenceEquals(card1, null) || ReferenceEquals(card2, null))
+                return false;
+            return card1._cvcCode == card2._cvcCode;
+        }
+
+        public static bool operator !=(CreditCard card1, CreditCard card2)
+        {
+            return !(card1 == card2);
+        }
+
+
+        public static bool operator <(CreditCard card1, CreditCard card2)
+        {
+            if (ReferenceEquals(card1, null) || ReferenceEquals(card2, null))
+                throw new ArgumentException("Objects must not be null");
+
+            return card1._balance < card2._balance;
+        }
+
+
+        public static bool operator >(CreditCard card1, CreditCard card2)
+        {
+            if (ReferenceEquals(card1, null) || ReferenceEquals(card2, null))
+                throw new ArgumentException("Objects must not be null");
+
+            return card1._balance > card2._balance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            CreditCard other = (CreditCard)obj;
+            return _cardNumber == other.CardNumber && _cvcCode
+                 == other.CVCCode && _balance == other.Balance;
+        }
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+    Створiть та опишiть клас «Кредитна картка». 
+
+    Додайте до вже створеного класу інформацію про суму грошей на картці. 
+
+    Виконайте перевантаження 
+
+    + (для збільшення суми грошей на вказану кількість), 
+
+    – (для зменшення суми грошей на вказану кількість), 
+
+    == (перевірка на рівність CVC коду), 
+
+    < і > (перевірка на меншу чи більшу кількість суми грошей), 
+
+    != і Equals. 
+
+    Використовуйте механізм властивостей полів класу.
+*/
+
+using Task_4;
+
+CreditCard card1 = new CreditCard("1234567890123456", 123, 1000.0m);
+CreditCard card2 = new CreditCard("9876543210987654", 456, 1500.0m);
+
+Console.WriteLine("Initial balance on the cards:");
+Console.WriteLine(card1.CardNumber + ": " + card1.Balance);
+Console.WriteLine(card2.CardNumber + ": " + card2.Balance);
+
+card1 += 500.0m;
+card2 -= 200.0m;
+
+Console.WriteLine("Changed balance on cards:");
+Console.WriteLine(card1.CardNumber + ": " + card1.Balance);
+Console.WriteLine(card2.CardNumber + ": " + card2.Balance);
+
+if (card1 == card2)
+    Console.WriteLine("The CVC codes of the cards are the same");
+else
+    Console.WriteLine("CVC codes of cards are different");
+
+if (card1 < card2)
+    Console.WriteLine(card1.CardNumber + " has a smaller balance than " + card2.CardNumber);
+else if (card1 > card2)
+    Console.WriteLine(card1.CardNumber + " has a larger balance than " + card2.CardNumber);
+
+
+Console.WriteLine("Equals: " + card1.Equals(card2));
