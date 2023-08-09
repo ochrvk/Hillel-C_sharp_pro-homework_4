bded20972f20299722bc240da46d1ae058fb0cec
Oleksandr Cherevko <oc.oleks.cherevko@gmail.com>
2023-08-09 20:35:25 +0300

    task2 has been done

---
+++ Task_2/Matrix.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task_2
+{
+    public class Matrix
+    {
+        public Matrix(int rows, int columns)
+        {
+            _rows = rows;
+            _columns = columns;
+            _array = new int[rows, columns];
+        }
+
+
+
+        private int _rows;
+        private int _columns;
+        private int[,] _array;
+
+        public int Rows
+        {
+            get { return _rows; }
+        }
+
+        public int Columns
+        {
+            get { return _columns; }
+        }
+
+        public int this[int row, int column]
+        {
+            get
+            {
+                if (row >= 0 && row < _array.GetLength(0) && column >= 0 && column < _array.GetLength(1))
+                    return _array[row, column];
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (row >= 0 && row < _array.GetLength(0) && column >= 0 && column < _array.GetLength(1))
+                    _array[row, column] = value;
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            if (m1._rows != m2._columns || m1._columns != m2._columns)
+                throw new ArgumentException("The matrixes have different sizes");
+
+            Matrix result = new Matrix(m1._rows, m1._columns);
+            for (int i = 0; i < m1._rows; i++)
+            {
+                for (int j = 0; j < m1._columns; j++)
+                {
+                    result[i, j] = m1[i, j] + m2[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            if (m1._rows != m2._rows || m1._columns != m2._columns)
+                throw new ArgumentException("The matrixes have different sizes");
+
+            Matrix result = new Matrix(m1._rows, m1._columns);
+            for (int i = 0; i < m1._rows; i++)
+            {
+                for (int j = 0; j < m1._columns; j++)
+                {
+                    result[i, j] = m1[i, j] - m2[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix, int scalar)
+        {
+            Matrix result = new Matrix(matrix._rows, matrix._columns);
+            for (int i = 0; i < matrix._rows; i++)
+            {
+                for (int j = 0; j < matrix._columns; j++)
+                {
+                    result[i, j] = matrix[i, j] * scalar;
+                }
+            }
+            return result;
+        }
+
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if (m1.Columns != m2.Rows)
+                throw new ArgumentException("The number of columns of the first matrix must be equal to the number of rows of the second matrix");
+
+            Matrix result = new Matrix(m1.Rows, m2.Columns);
+            for (int i = 0; i < m1.Rows; i++)
+            {
+                for (int j = 0; j < m2.Columns; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < m1.Columns; k++)
+                    {
+                        sum += m1[i, k] * m2[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+            return result;
+        }
+
+        public static bool operator ==(Matrix m1, Matrix m2)
+        {
+            if (ReferenceEquals(m1, null) || ReferenceEquals(m2, null))
+                return false;
+            if (m1._rows != m2._rows || m1._columns != m2._columns)
+                return false;
+
+            for (int i = 0; i < m1._rows; i++)
+            {
+                for (int j = 0; j < m1._columns; j++)
+                {
+                    if (m1[i, j] != m2[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !=(Matrix m1, Matrix m2)
+        {
+            return !(m1 == m2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Matrix other = (Matrix)obj;
+
+            return _rows == other.Rows || _columns == other.Columns;
+        }
+
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+    Створiть та опишiть клас «Матриця». 
+
+    Виконайте перевантаження 
+
+    (для додавання матриць), 
+
+    (для віднімання матриць).
+
+    (множення матриць одна на одну, множення матриці на число), 
+
+    (перевірка матриць на рівність), 
+
+    != і Equals. 
+
+    Використовуйте механізм властивостей полів класу і механізм індексаторів. 
+ */
+using Task_2;
+
+Matrix matrix1 = new Matrix(2, 2);
+matrix1[0, 0] = 1;
+matrix1[0, 1] = 2;
+matrix1[1, 0] = 3;
+matrix1[1, 1] = 4;
+
+Matrix matrix2 = new Matrix(2, 2);
+matrix2[0, 0] = 5;
+matrix2[0, 1] = 6;
+matrix2[1, 0] = 7;
+matrix2[1, 1] = 8;
+
+Matrix sum = matrix1 + matrix2;
+Console.WriteLine("Sum:");
+for (int i = 0; i < sum.Rows; i++)
+{
+    for (int j = 0; j < sum.Columns; j++)
+    {
+        Console.Write(sum[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Matrix difference = matrix1 - matrix2;
+Console.WriteLine("Difference:");
+for (int i = 0; i < difference.Rows; i++)
+{
+    for (int j = 0; j < difference.Columns; j++)
+    {
+        Console.Write(difference[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Matrix result_1 = matrix1 * 2;
+Console.WriteLine("Matrix multiplied by 2:");
+for (int i = 0; i < result_1.Rows; i++)
+{
+    for (int j = 0; j < result_1.Columns; j++)
+    {
+        Console.Write(result_1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Matrix result_2 = matrix1 * matrix2;
+Console.WriteLine("Matrix multiplied by a matrix:");
+for (int i = 0; i < result_2.Rows; i++)
+{
+    for (int j = 0; j < result_2.Columns; j++)
+    {
+        Console.Write(result_2[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+if (matrix1 == matrix2)
+    Console.WriteLine("(==)Matrixes are equal");
+else
+    Console.WriteLine("(==)Matrixes aren't equal");
+
+if (matrix1 != matrix2)
+    Console.WriteLine("(!=)Matrixes aren't equal");
+else
+    Console.WriteLine("(!=)Matrixes are equal");
+
+
+Matrix test = new Matrix(3, 4);
+
+Console.WriteLine("Equals: " + matrix1.Equals(matrix2));
+Console.WriteLine("Equals: " + matrix1.Equals(test));
+
